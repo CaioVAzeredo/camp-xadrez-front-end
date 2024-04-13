89139f006ff4e48e10eaeb8547f80bfd304eb5e2
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-04-13 18:56:06 -0300

    sem auterações

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,21 +5,26 @@ import './Formulario.css'
 
 
 const Formulario = (props) => {
+
     const aoSalvar = (e) => {
         e.preventDefault()
-        console.log('atualizado')
     }
-
-
+    /* 
+        const aoAtualizar = (e) => {
+            console.log('atualizado')
+            window.location.reload();
+        } */
     return (
         <div className='formulario'>
             < form onSubmit={aoSalvar} className='form'>
                 <Podium data={props.podium} />
-                <h2> JOGADORES </h2>
+                <h2 > JOGADORES </h2>
                 <div className='divJogadores'>
-                    {props.topRestante.map((aluno, i) => <Jogadores key={aluno.id} nome={aluno.nome} pontos={aluno.ponto} indice={i}/>)}
+                    {props.topRestante.map((aluno, i) => <Jogadores key={aluno.id} nome={aluno.nome} pontos={aluno.ponto} indice={i} />)}
                 </div>
-                <Botao texto={'Atualizar'} />
+                <div className='botoes' >
+                    <Botao texto={'Atualizar'} />
+                </div>
             </form >
         </div>
     )
