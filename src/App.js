d9e59036dbc91cb64db5911eac2502a068c5f780
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-04-23 23:04:38 -0300

    adição de exemplo de consumo de api

--- src/App.js
+++ src/App.js
@@ -152,3 +152,55 @@ function App() {
 }
 
 export default App;
+
+/*Consumindo API 
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [endereco, setEndereco] = useState({})
+
+  function manipularEndereco(evento) {
+    const cep = evento.target.value
+    setEndereco({
+      cep
+    })
+
+    if (cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setEndereco({
+            cep: dados.cep,
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf,
+          })
+        })
+    }
+
+
+    
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input placeholder='Digite o cep' onChange={manipularEndereco} />
+        <ul>
+        <li>CEP: {endereco.cep}</li>
+        <li>rua: {endereco.rua}</li>
+        <li>bairro: {endereco.bairro}</li>
+        <li>cidade: {endereco.cidade}</li>
+        <li>estado: {endereco.estado}</li>
+
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ */
