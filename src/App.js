985cc0ff27d1396e38a684e3ac395b2cb1dbb2db
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-07-11 16:48:07 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React, { useState, useEffect } from 'react';
 
 const AlunosList = () => {
-  const [alunos, setAlunos] = useState([]);
+    const [alunos, setAlunos] = useState([]);
 
-  useEffect(() => {
-    const fetchAlunos = async () => {
-      try {
-        const response = await fetch('http://18.231.177.78:3333/alunos/');
-        const dados = await response.json();
-        setAlunos(dados);
-      } catch (error) {
-        console.error('Erro ao buscar alunos:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchAlunos = async () => {
+            try {
+                const response = await fetch('http://18.231.177.78:3333/alunos/');
+                const dados = await response.json();
+                setAlunos(dados);
+            } catch (error) {
+                console.error('Erro ao buscar alunos:', error);
+            }
+        };
 
-    fetchAlunos();
-  }, []); 
+        fetchAlunos();
+    }, []);
 
-  return (
-    <div className='container'>
-      <h1>Lista de Alunos</h1>
-      <div>
-        {alunos.map(aluno => (
-          <div key={aluno.id} className="aluno">
-            <h3>{aluno.nome}</h3>
-            <p><strong>Ponto:</strong> {aluno.ponto}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        
+        <div className='container'>
+
+            <div className='primeiros-tres'>
+                {alunos.slice(0, 3).map((aluno, index) => (
+                    <div key={aluno.id} className="aluno-primeiros-tres">
+                        <h3>{aluno.nome}</h3>
+                        <p>{index + 1}°</p>
+                        <p><strong>Ponto:</strong> {aluno.ponto}</p>
+                    </div>
+                ))}
+            </div>
+            <div className='resto-dos-alunos'>
+                {alunos.slice(3).map((aluno, index) => (
+                    <div key={aluno.id} className="aluno-resto-dos-alunos">
+                        <p>{index + 4}°</p><h3>{aluno.nome} </h3> <p><strong>Ponto:</strong> {aluno.ponto}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default AlunosList;
